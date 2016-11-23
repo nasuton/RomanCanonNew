9f0096b42c58d32ab3a315483ea35e2b30bc88e6
nasuton <lovetennis_airu@yahoo.co.jp>
2016-11-23 22:34:21 +0900

    エネミーの動きの追加

---
+++ Assets/Scripts/Katou/probability.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class probability
+{
+	public static T enemy_election<T>(Dictionary <T, int> target)
+    {
+        float total = 0;
+        
+        foreach (float per in target.Values)
+        {
+            total += per;
+        }
+
+        float rand = Random.Range(0, total);
+
+        foreach(KeyValuePair<T, int> pair in target)
+        {
+            rand -= pair.Value;
+
+            if(rand < 0)
+            {
+                return pair.Key;
+            }
+        }
+
+        return new List<T>(target.Keys)[0];
+    }
+}
---
+++ Assets/Scripts/Katou/test_enemy/enemy4_cs.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy4_cs : MonoBehaviour
+{
+
+    Vector3 target;
+
+    bool jumpflug;
+    bool execution;
+
+    Rigidbody body;
+
+    [SerializeField]
+    float jump_power;
+
+    [SerializeField]
+    private float maxtime = 2;
+
+    float nowtime;
+
+    [SerializeField]
+    private float speed;
+
+    void Start()
+    {
+        target = Vector3.zero;
+        jumpflug = false;
+        execution = false;
+        body = GetComponent<Rigidbody>();
+        jump_power = 500.0f;
+        nowtime = 0.0f;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if (!jumpflug)
+        {
+            nowtime += Time.deltaTime;
+            if (maxtime <= nowtime)
+            {
+                jumpflug = true;
+            }
+        }
+        else if (jumpflug)
+        {
+            if (!execution)
+            {
+                During_Jump();
+            }
+        }
+    }
+
+    void During_Jump()
+    {
+        body.AddForce(Vector3.up * jump_power);
+        execution = true;
+        nowtime = 0.0f;
+    }
+
+    void Attack()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        jumpflug = false;
+        execution = false;
+    }
+
+}
