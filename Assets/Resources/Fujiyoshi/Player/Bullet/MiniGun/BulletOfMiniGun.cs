45a00600d47a1c969dae9edd8a55e6e658638322
PTxDE*SS <desspertsouji@yahoo.co.jp>
2016-11-23 22:00:18 +0900

    爆破追加

---
+++ Assets/Resources/Fujiyoshi/Player/Bullet/MiniGun/BulletOfMiniGun.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletOfMiniGun : MonoBehaviour
+{
+    [SerializeField]
+    GameObject effect;
+    [SerializeField]
+    GameObject sphereCollider;
+    void Awake()
+    {
+        this.GetComponent<BulletDamege>().Damege = GameObject.Find("WeaponStatus").GetComponent<WeaponStatusManager>().Status[0];
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (GameObject.Find("RomanBar").GetComponent<RomanGauge>().roman_mode == true &&
+              GameObject.Find("WeaponStatus").GetComponent<WeaponStatusManager>().roman_type.brust == WeaponStatusManager.RomanType.BurstType.rocket)
+        {
+            GameObject.Instantiate(effect);
+            GameObject.Instantiate(sphereCollider);
+            effect.transform.localPosition = other.transform.localPosition;
+            sphereCollider.transform.localPosition = other.transform.localPosition;
+        }
+    }
+}
